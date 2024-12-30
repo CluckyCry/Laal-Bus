7c19b03f0f16af4a4b794c7586874284246db107
Chain-web-hash <extremebinary6@gmail.com>
2024-12-30 19:44:17 +0500

    yes

--- frontend/src/assets/leafletIcons.ts
+++ frontend/src/assets/leafletIcons.ts
@@ -1,11 +1,16 @@
+// icon imports
+import markerIcon from "./marker-icon.png" 
+import markerShadow from "./marker-shadow.png"
+import markerIcon2x from "./marker-icon-2x.png"
+
 import L from 'leaflet';
 
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconUrl: './marker-icon.png',
-  iconRetinaUrl: './marker-icon-2x.png',
-  shadowUrl: './marker-shadow.png',
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerShadow,
+  shadowUrl: markerIcon2x
 });
 
 export default L;
